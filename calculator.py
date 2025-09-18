51d57764c04b53aa518a9a51f82994f900a8bafa
Shamezan <shmeezann@gmail.com>
2025-09-18 14:16:56 +0800

    Update calculator.py
    I've added the introduction and comments so that anyone can understand it.

--- calculator.py
+++ calculator.py
@@ -1,16 +1,21 @@
+## Hi! I created a simple calculator project
+## Only involved basic arithmetic like addition, subtraction, multiplication, and division
+## Next time, I may create an advanced calculator
+## That can do complex maths like differential equations
 
+## A function to display the operations menu
 def display_calc(calculator):
     print('\n--- Calculator ---')
     for operation_number, operation_name in calculator.items():
         print(f'{operation_number}. {operation_name['operation']}')
     print('------------------')
-
+## addition operation
 def additional():
     first_number = int(input('Enter the first number: '))
     second_number = int(input('Enter the second number: '))
     add = int(first_number) + int(second_number)
     print('The answer is: ' + str(add))
-
+## subtract operation
 def substract():
     first_number = int(input('Enter first number: '))
     second_number = int(input('Enter second number: '))
@@ -22,13 +27,13 @@ def substract():
         print('The answer is: ' + str(subs))
     else:
         print('error')
- 
+ ## multiply operation
 def multiply():
     first_number = int(input('Enter first number: '))
     second_number = int(input('Enter second number: '))
     mult = first_number * second_number
     print('The answer is: ' + str(mult))
-
+## division operation
 def divide():
     first_number = int(input('Enter first number: '))
     second_number = int(input('Enter second number: '))
@@ -37,8 +42,7 @@ def divide():
     else:
         div = first_number / second_number
         print('The answer is: ' + str(div))
-
-
+## ask user which operation they want
 def calculate(calculator):
     while True:
         task = input("Please enter an operation number or 'done' to finish: ").lower()
@@ -54,7 +58,7 @@ def calculate(calculator):
             divide()
         else:
             print('Error')
-    
+## The main function
 def main():
     """Main function for calculator"""
     calculator = {
@@ -66,10 +70,11 @@ def main():
     print('\nCalculate simple arithmetic')
     display_calc(calculator)
     user_task = calculate(calculator)
-
+## Only run the main function
 if __name__ == '__main__':
     main()
     
     
     
      
+
